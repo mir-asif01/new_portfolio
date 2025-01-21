26bc792778bf6a12a0e676b3091e9594f4843d00
mir-asif01 <asifbsb2002@gmail.com>
2025-01-21 17:59:49 +0600

    Update HeroSection styles for improved readability and consistency

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -11,21 +11,21 @@ export function GridBackgroundDemo() {
       {/* Radial gradient for the container to give a faded look */}
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <div className="flex justify-between items-center mb-10">
-        <h1 className="font-semibold text-gray-900 z-20">Asif</h1>
-        <button className="border border-sky-400 px-2 py-1 rounded-full text-xs z-20 flex items-center gap-1">
+        <h1 className="font-semibold z-20">Asif</h1>
+        <button className="border border-sky-400 px-2 py-1 rounded-full text-sm z-20 flex items-center gap-1">
             <span>twitter</span>
             <GoArrowUpRight/>
         </button>
       </div>
       <div className="z-20">
-        <p className="flex justify-center items-center mb-2 text-xs z-20">
+        <p className="flex justify-center items-center mb-2 text-sm z-20">
             <span className="flex items-center justify-center gap-1">
                 <span className="bg-green-500 h-3 w-3 rounded-full animate-pulse"></span>
                 open for <FlipWords className="font-semibold text-green-500" words={words}/>
             </span>
         </p>
         <h1 className="text-2xl text-center z-20">Crafting Digital World</h1>
-        <p className="text-sm font-semibold text-center mt-7 z-20">
+        <p className="text-md font-semibold text-center mt-7 z-20">
             <Highlight className="py-1 px-2">Creative Developer</Highlight>
              specializing in modern web solutions using cutting-edge technologies
         </p>
