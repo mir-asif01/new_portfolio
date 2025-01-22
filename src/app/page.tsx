1055f77990156da47cace11a3ccb75644d61d9b0
mir-asif01 <asifbsb2002@gmail.com>
2025-01-22 16:15:03 +0600

    Remove @tabler/icons-react dependency and add WhatIDo component for showcasing services

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import { GridBackgroundDemo } from "@/components/HeroSection";
 import ProjectsComponent from "@/components/Projects";
 import Skills from "@/components/Skills";
+import WhatIDo from "@/components/WhatIDo";
 
 export default function Home() {
   return (
     <section>
       <GridBackgroundDemo/>
       <ProjectsComponent/>
-      <Skills/>      
+      <Skills/>
+      <WhatIDo/>
     </section>
   );
 }
