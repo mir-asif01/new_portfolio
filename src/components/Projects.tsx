ccfd6ed32228104573c687f6a6ce440058c3d190
mir-asif01 <asifbsb2002@gmail.com>
2025-02-22 10:09:05 +0600

    IProject type added while maping over the projects array

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,11 +1,12 @@
 import { projects } from "@/lib/projects";
 import ProjectCard from "./ui/project-card";
+import { IProject } from "@/types/types";
 export default function ProjectsComponent() {
   return (
     <section className="pt-10 dark:bg-gray-900 md:container mx-auto">
       <h1 className="text-center text-xl mb-5 md:text-5xl">Projects</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 p-7 gap-10">
-        {projects.map((project: any) => (
+        {projects.map((project: IProject) => (
           <ProjectCard
             key={project.id}
             image={project?.image}
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,19 +1,19 @@
-import { StaticImageData } from "next/image"
-import { IconType } from "react-icons"
+import { StaticImageData } from "next/image";
+import { IconType } from "react-icons";
 
 export interface IProject {
-    id : string
-    image : StaticImageData
-    title : string
-    description : string
-    tags? : string[],
-    techStack : string[],
-    client_repo? : string,
-    server_repo? : string,
-    live? : string
+  id: string;
+  image: StaticImageData;
+  title: string;
+  description: string;
+  tags: string[];
+  techStack: string[];
+  client_repo?: string;
+  server_repo?: string;
+  live?: string;
 }
 
 export interface ISkill {
-    name : string
-    logo : IconType
+  name: string;
+  logo: IconType;
 }
